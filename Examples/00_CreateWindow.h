3c9d70d9e20a240e9612ee4018743790b4839e43
Zvonimir Pervan <z.pervan65@gmail.com>
2021-12-05 19:27:31 +0100

    Add window creation example

---
+++ Examples/00_CreateWindow.h
@@ -0,0 +1,61 @@
+#ifndef OPENGLDOJO_00_CREATEWINDOW_H
+#define OPENGLDOJO_00_CREATEWINDOW_H
+
+
+#include <iostream>
+
+#include "ThirdParty/glad/glad.h"
+#include "ThirdParty/GLFW/glfw3.h"
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void _00_CreateWindow()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // Input
+        processInput(window);
+
+        // Rendering
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+}
+
+#endif  // OPENGLDOJO_00_CREATEWINDOW_H
--- main.cpp
+++ main.cpp
@@ -1,41 +1,8 @@
-#include "ThirdParty/glad/glad.h"
-#include "ThirdParty/GLFW/glfw3.h"
-
-#include <iostream>
+#include "Examples/00_CreateWindow.h"
 
 int main()
 {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-
-    glViewport(0, 0, 800, 600);
-
-    while(!glfwWindowShouldClose(window))
-    {
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-
+    // Basic "Hello, World!" example to create a blank window
+    _00_CreateWindow();
     return 0;
 }
